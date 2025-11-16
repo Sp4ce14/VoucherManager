8e0275b192a69454386346107c16eed6a5b4bd97
sp4ce14 <aw136061@gmail.com>
2025-11-17 00:40:07 +0500

    fdfd

---
+++ Backend/VoucherManager/Dtos/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace VoucherManager.Dtos
+{
+    public class LoginDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Backend/VoucherManager/Repositories/IAccountsRepository.cs
+++ Backend/VoucherManager/Repositories/IAccountsRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace VoucherManager.Repositories
+﻿using Microsoft.AspNetCore.Identity;
+using VoucherManager.Dtos;
+
+namespace VoucherManager.Repositories
 {
     public interface IAccountsRepository
     {
+        Task<AuthResultDto> SignUpAsync(SignUpDto signUpDto);
     }
 }
